7ea21c7bc5b11538fa639bac3148595132c628a2
Ali-reza Mohammadi-Nejad <mszam12@exmail.nottingham.ac.uk>
2018-11-26 16:07:05 +0000

    Log file compatibility is applied

--- BRC_functional_pipeline/scripts/Temporal_Filtering.sh
+++ BRC_functional_pipeline/scripts/Temporal_Filtering.sh
@@ -7,6 +7,8 @@
 #
 set -e
 
+source $BRC_GLOBAL_SCR/log.shlib  # Logging related functions
+
 # function for parsing options
 getopt1()
 {
@@ -27,12 +29,15 @@ WD=`getopt1 "--workingdir" $@`
 Input_fMRI=`getopt1 "--infmri" $@`
 Temp_Filter_Cutoff=`getopt1 "--tempfiltercutoff" $@`
 OutfMRI=`getopt1 "--outfmri" $@`
+LogFile=`getopt1 "--logfile" $@`
 
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo "+                                                                        +"
-echo "+                       START: Temporal Filtering                        +"
-echo "+                                                                        +"
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+log_SetPath "${LogFile}"
+
+log_Msg 3 "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+log_Msg 3 "+                                                                        +"
+log_Msg 3 "+                       START: Temporal Filtering                        +"
+log_Msg 3 "+                                                                        +"
+log_Msg 3 "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
 RepetitionTime=`${FSLDIR}/bin/fslval ${Input_fMRI} pixdim4 | cut -d " " -f 1`
 
@@ -43,11 +48,11 @@ hp_sigma_vol=`echo "scale=6; (($hp_sigma_sec / $RepetitionTime))" | bc`
 
 ${FSLDIR}/bin/fslmaths ${Input_fMRI} -bptf $hp_sigma_vol -1 -add ${WD}/tempMean  ${WD}/${OutfMRI}
 
-echo ""
-echo "                         END: Temporal Filtering"
-echo "                    END: `date`"
-echo "=========================================================================="
-echo "                             ===============                              "
+log_Msg 3 ""
+log_Msg 3 "                         END: Temporal Filtering"
+log_Msg 3 "                    END: `date`"
+log_Msg 3 "=========================================================================="
+log_Msg 3 "                             ===============                              "
 
 ################################################################################################
 ## Cleanup
