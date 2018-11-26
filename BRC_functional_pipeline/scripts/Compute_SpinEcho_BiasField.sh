030652c1792e3accb22eed1cb3c2945ad94701ef
Ali-reza Mohammadi-Nejad <mszam12@exmail.nottingham.ac.uk>
2018-11-26 16:09:20 +0000

    Log file compatibility and some folder/file revision is applied

--- BRC_functional_pipeline/scripts/Compute_SpinEcho_BiasField.sh
+++ BRC_functional_pipeline/scripts/Compute_SpinEcho_BiasField.sh
@@ -8,6 +8,8 @@
 
 set -e
 
+source $BRC_GLOBAL_SCR/log.shlib  # Logging related functions
+
 # function for parsing options
 getopt1()
 {
@@ -21,6 +23,7 @@ getopt1()
     done
 }
 
+# parse arguments
 WD=`getopt1 "--workingdir" "$@"`
 SubjectFolder=`getopt1 "--subjectfolder" "$@"` #replaces StudyFolder and Subject
 fMRIName=`getopt1 "--fmriname" "$@"`
@@ -28,6 +31,28 @@ fMRIName=`getopt1 "--fmriname" "$@"`
 #SubCorticalLUT=`getopt1 "--subcorticallut" "$@"`
 SmoothingFWHM=`getopt1 "--smoothingfwhm" "$@"`
 InputDir=`getopt1 "--inputdir" "$@"`
+T1wBrainImage=`getopt1 "--t1brain" "$@"`
+GMseg=`getopt1 "--gmseg" "$@"`
+LogFile=`getopt1 "--logfile" $@`
+
+log_SetPath "${LogFile}"
+
+log_Msg 3 "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+log_Msg 3 "+                                                                        +"
+log_Msg 3 "+              START: Receive coil bias field correction                 +"
+log_Msg 3 "+                                                                        +"
+log_Msg 3 "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+log_Msg 2 "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+log_Msg 2 "WD:$WD"
+log_Msg 2 "SubjectFolder:$SubjectFolder"
+log_Msg 2 "fMRIName:$fMRIName"
+log_Msg 2 "SmoothingFWHM:$SmoothingFWHM"
+log_Msg 2 "InputDir:$InputDir"
+log_Msg 2 "T1wBrainImage:$T1wBrainImage"
+log_Msg 2 "GMseg:$GMseg"
+log_Msg 2 "LogFile:$LogFile"
+log_Msg 2 "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
 Sigma=`echo "$SmoothingFWHM / ( 2 * ( sqrt ( 2 * l ( 2 ) ) ) )" | bc -l`
 
@@ -38,17 +63,17 @@ ${FSLDIR}/bin/fslmaths ${InputDir}/PhaseOne_gdc_dc.nii.gz -add ${InputDir}/Phase
 ${FSLDIR}/bin/imcp ${InputDir}/SBRef_dc.nii.gz ${WD}/GRE.nii.gz
 
 ${FSLDIR}/bin/fslmaths ${WD}/SpinEchoMean.nii.gz -div ${WD}/GRE.nii.gz ${WD}/SEdivGRE.nii.gz
-${FSLDIR}/bin/fslmaths ${WD}/SEdivGRE.nii.gz -mas ${T1wFolder}/preprocess/T1_biascorr_brain_mask.nii.gz ${WD}/SEdivGRE_brain.nii.gz
+${FSLDIR}/bin/fslmaths ${WD}/SEdivGRE.nii.gz -mas ${T1wBrainImage}_mask.nii.gz ${WD}/SEdivGRE_brain.nii.gz
 
 ${FSLDIR}/bin/fslmaths ${WD}/SEdivGRE_brain.nii.gz -thr 1.25 -uthr 2.2 ${WD}/SEdivGRE_brain_thr.nii.gz
 ${FSLDIR}/bin/fslmaths ${WD}/SEdivGRE_brain_thr.nii.gz -bin ${WD}/SEdivGRE_brain_thr_roi.nii.gz
 ${FSLDIR}/bin/fslmaths ${WD}/SEdivGRE_brain_thr.nii.gz -s 5 ${WD}/SEdivGRE_brain_thr_s5.nii.gz
 ${FSLDIR}/bin/fslmaths ${WD}/SEdivGRE_brain_thr_roi.nii.gz -s 5 ${WD}/SEdivGRE_brain_thr_roi_s5.nii.gz
-${FSLDIR}/bin/fslmaths ${WD}/SEdivGRE_brain_thr_s5.nii.gz -div ${WD}/SEdivGRE_brain_thr_roi_s5.nii.gz -mas ${T1wFolder}/preprocess/T1_biascorr_brain_mask.nii.gz ${WD}/SEdivGRE_brain_bias.nii.gz
+${FSLDIR}/bin/fslmaths ${WD}/SEdivGRE_brain_thr_s5.nii.gz -div ${WD}/SEdivGRE_brain_thr_roi_s5.nii.gz -mas ${T1wBrainImage}_mask.nii.gz ${WD}/SEdivGRE_brain_bias.nii.gz
 
-${FSLDIR}/bin/fslmaths ${WD}/SpinEchoMean.nii.gz -mas ${T1wFolder}/preprocess/T1_biascorr_brain_mask.nii.gz -div ${WD}/SEdivGRE_brain_bias.nii.gz ${WD}/SpinEchoMean_brain_BC.nii.gz
+${FSLDIR}/bin/fslmaths ${WD}/SpinEchoMean.nii.gz -mas ${T1wBrainImage}_mask.nii.gz -div ${WD}/SEdivGRE_brain_bias.nii.gz ${WD}/SpinEchoMean_brain_BC.nii.gz
 
-${FSLDIR}/bin/fslmaths ${WD}/GRE.nii.gz -mas ${T1wFolder}/preprocess/T1_biascorr_brain_mask.nii.gz -div ${WD}/SpinEchoMean_brain_BC.nii.gz ${WD}/SE_BCdivGRE_brain.nii.gz
+${FSLDIR}/bin/fslmaths ${WD}/GRE.nii.gz -mas ${T1wBrainImage}_mask.nii.gz -div ${WD}/SpinEchoMean_brain_BC.nii.gz ${WD}/SE_BCdivGRE_brain.nii.gz
 
 ${FSLDIR}/bin/fslmaths ${WD}/SE_BCdivGRE_brain.nii.gz -uthr 0.5 -bin ${WD}/Dropouts.nii.gz
 ${FSLDIR}/bin/fslmaths ${WD}/Dropouts.nii.gz -dilD -s ${Sigma} ${WD}/${fMRIName}_dropouts.nii.gz
@@ -60,22 +85,16 @@ ${FSLDIR}/bin/fslmaths ${WD}/Dropouts.nii.gz -binv ${WD}/Dropouts_inv.nii.gz
 #${FSLDIR}/bin/fslmaths ${WD}/CorticalGreyMatter.nii.gz -bin ${WD}/CorticalGreyMatter.nii.gz
 #${FSLDIR}/bin/fslmaths ${WD}/CorticalGreyMatter.nii.gz -add ${WD}/SubcorticalGreyMatter.nii.gz -bin ${WD}/AllGreyMatter.nii.gz
 
-#Check GM segment exist or no
-if [ `$FSLDIR/bin/imtest $T1wFolder/seg/tissue/multi_chan/T1_mc_GM` = 1 ] ; then
-    GMseg="$T1wFolder/seg/tissue/multi_chan/T1_mc_GM"
-elif [[ `$FSLDIR/bin/imtest $T1wFolder/seg/tissue/sing_chan/T1_pve_GM` = 1 ]]; then
-    GMseg="$T1wFolder/seg/tissue/sing_chan/T1_pve_GM"
-fi
 
 ${FSLDIR}/bin/fslmaths ${WD}/GRE.nii.gz -mas ${GMseg} -mas ${WD}/Dropouts_inv.nii.gz -bin ${WD}/GRE_greyroi.nii.gz
 ${FSLDIR}/bin/fslmaths ${WD}/GRE.nii.gz -mas ${WD}/GRE_greyroi.nii.gz -s 5 ${WD}/GRE_grey_s5.nii.gz
 ${FSLDIR}/bin/fslmaths ${WD}/GRE_greyroi.nii.gz -s 5 ${WD}/GRE_greyroi_s5.nii.gz
-${FSLDIR}/bin/fslmaths ${WD}/GRE_grey_s5.nii.gz -div ${WD}/GRE_greyroi_s5.nii.gz -mas ${GMseg} -dilall -mas ${T1wFolder}/preprocess/T1_biascorr_brain_mask.nii.gz ${WD}/GRE_bias_raw.nii.gz
+${FSLDIR}/bin/fslmaths ${WD}/GRE_grey_s5.nii.gz -div ${WD}/GRE_greyroi_s5.nii.gz -mas ${GMseg} -dilall -mas ${T1wBrainImage}_mask.nii.gz ${WD}/GRE_bias_raw.nii.gz
 
 ${FSLDIR}/bin/fslmaths ${WD}/GRE_bias_raw.nii.gz -bin ${WD}/GRE_bias_roi.nii.gz
 ${FSLDIR}/bin/fslmaths ${WD}/GRE_bias_raw.nii.gz -s 5 ${WD}/GRE_bias_raw_s5.nii.gz
 ${FSLDIR}/bin/fslmaths ${WD}/GRE_bias_roi.nii.gz -s 5 ${WD}/GRE_bias_roi_s5.nii.gz
-${FSLDIR}/bin/fslmaths ${WD}/GRE_bias_raw_s5.nii.gz -div ${WD}/GRE_bias_roi_s5.nii.gz -mas ${T1wFolder}/preprocess/T1_biascorr_brain_mask.nii.gz ${WD}/GRE_bias.nii.gz
+${FSLDIR}/bin/fslmaths ${WD}/GRE_bias_raw_s5.nii.gz -div ${WD}/GRE_bias_roi_s5.nii.gz -mas ${T1wBrainImage}_mask.nii.gz ${WD}/GRE_bias.nii.gz
 Mean=`fslstats ${WD}/GRE_bias.nii.gz -M`
 ${FSLDIR}/bin/fslmaths ${WD}/GRE_bias.nii.gz -div ${Mean} ${WD}/${fMRIName}_sebased_bias.nii.gz
 ${FSLDIR}/bin/fslmaths ${WD}/${fMRIName}_sebased_bias.nii.gz -ing 10000 ${WD}/${fMRIName}_sebased_reference.nii.gz
@@ -83,8 +102,11 @@ ${FSLDIR}/bin/fslmaths ${WD}/${fMRIName}_sebased_bias.nii.gz -ing 10000 ${WD}/${
 ${FSLDIR}/bin/fslmaths ${WD}/${fMRIName}_sebased_bias.nii.gz -dilM -dilM ${WD}/sebased_bias_dil.nii.gz
 ${FSLDIR}/bin/fslmaths ${WD}/${fMRIName}_sebased_reference.nii.gz -dilM -dilM ${WD}/sebased_reference_dil.nii.gz
 
-echo "END"
-echo " END: `date`"
+log_Msg 3 ""
+log_Msg 3 "                 END: Receive coil bias field correction"
+log_Msg 3 "                    END: `date`"
+log_Msg 3 "=========================================================================="
+log_Msg 3 "                             ===============                              "
 
 ################################################################################################
 ## Cleanup
