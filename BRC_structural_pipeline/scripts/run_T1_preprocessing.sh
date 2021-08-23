2abaa7a5a780ebf8f2c569138c080ce4d6e310d1
Ali-reza Mohammadi-Nejad <mszam12@exmail.nottingham.ac.uk>
2021-08-23 16:04:26 +0100

    Revert "Revert "Two paths are defined for ANTs and C3D softwarres""
    This reverts commit 695046380d93e0f774ee57f7be5ee5bdabedd578.

--- BRC_structural_pipeline/scripts/run_T1_preprocessing.sh
+++ BRC_structural_pipeline/scripts/run_T1_preprocessing.sh
@@ -35,6 +35,7 @@ FirstT1Folder=`getopt1 "--firstfolder" $@`
 SienaxTempFolder=`getopt1 "--sienaxtempfolder" $@`
 regTempT1Folder=`getopt1 "--regtempt1folder" $@`
 RegType=`getopt1 "--regtype" $@`
+fBET=`getopt1 "--fbet" $@`
 LogFile=`getopt1 "--logfile" $@`
 
 log_SetPath "${LogFile}"
@@ -57,6 +58,7 @@ log_Msg 2 "FirstT1Folder:$FirstT1Folder"
 log_Msg 2 "SienaxTempFolder:$SienaxTempFolder"
 log_Msg 2 "regTempT1Folder:$regTempT1Folder"
 log_Msg 2 "RegType:$RegType"
+log_Msg 2 "fBET:$fBET"
 log_Msg 2 "LogFile:$LogFile"
 log_Msg 2 "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
@@ -73,7 +75,7 @@ fi
 
 log_Msg 3 `date`
 log_Msg 3 "Run a (Recursive) brain extraction"
-${FSLDIR}/bin/bet ${WD}/T1_tmp ${WD}/T1_tmp_brain -R -m -f 0.56
+${FSLDIR}/bin/bet ${WD}/T1_tmp ${WD}/T1_tmp_brain -R -m -f ${fBET}
 
 log_Msg 3 `date`
 echo "Reduces the FOV"
@@ -86,7 +88,8 @@ log_Msg 3 "Registering to standard space (linear)"
 #Generate the actual affine from the orig_ud volume to the cut version we have now and combine it to have an affine matrix from orig_ud to MNI
 ${FSLDIR}/bin/flirt  -in ${WD}/T1 -ref ${WD}/T1_orig_ud -omat ${WD}/T1_to_T1_orig_ud.mat -schedule $FSLDIR/etc/flirtsch/xyztrans.sch
 ${FSLDIR}/bin/convert_xfm -omat ${WD}/T1_orig_ud_to_T1.mat -inverse ${WD}/T1_to_T1_orig_ud.mat
-${FSLDIR}/bin/convert_xfm -omat ${WD}/T1_to_MNI_linear.mat -concat ${WD}/T1_tmp2_tmp_to_std.mat ${WD}/T1_to_T1_orig_ud.mat
+#${FSLDIR}/bin/convert_xfm -omat ${WD}/T1_to_MNI_linear.mat -concat ${WD}/T1_tmp2_tmp_to_std.mat ${WD}/T1_to_T1_orig_ud.mat
+${FSLDIR}/bin/flirt -interp spline -dof 12 -in ${WD}/T1 -ref $FSLDIR/data/standard/MNI152_T1_1mm -omat ${WD}/T1_to_MNI_linear.mat -out ${WD}/T1_tmp3
 
 if [ $RegType == 2 ]; then
 
@@ -105,28 +108,78 @@ if [ $RegType == 2 ]; then
                         --iout=${WD}/T1_tmp4.nii.gz \
                         --interp=spline
 
+#    printf "2" > ${LogFile%/*}/RegType.txt
+
+#    log_Msg 3 `date`
+#    log_Msg 3 "Combine the transformations into one and then apply it."
+#    ${FSLDIR}/bin/convertwarp --ref=$FSLDIR/data/standard/MNI152_T1_1mm --premat=${WD}/T1_orig_ud_to_T1.mat --warp1=${WD}/T1_to_MNI_nonlin_field --out=${WD}/T1_orig_to_MNI_warp
+#    ${FSLDIR}/bin/applywarp --rel -i ${T1input} -r $FSLDIR/data/standard/MNI152_T1_1mm -w ${WD}/T1_orig_to_MNI_warp -o ${WD}/T1_brain_to_MNI --interp=spline
+
+elif [ $RegType == 3 ]; then
 
     log_Msg 3 `date`
-    log_Msg 3 "Combine the transformations into one and then apply it."
-    ${FSLDIR}/bin/convertwarp --ref=$FSLDIR/data/standard/MNI152_T1_1mm --premat=${WD}/T1_orig_ud_to_T1.mat --warp1=${WD}/T1_to_MNI_nonlin_field --out=${WD}/T1_orig_to_MNI_warp
-#    ${FSLDIR}/bin/applywarp --rel -i ${T1input} -r $FSLDIR/data/standard/MNI152_T1_1mm -w ${WD}/T1_orig_to_MNI_warp -o ${WD}/T1_brain_to_MNI --interp=spline
+    log_Msg 3 "Registering to standard space (non-linear)"
+    ${ANTSPATH}/antsRegistrationSyN.sh -d 3 \
+                                       -f ${FSLDIR}/data/standard/MNI152_T1_1mm.nii.gz \
+                                       -m ${WD}/T1.nii.gz \
+                                       -o ${WD}/SynOut \
+                                       -n 48 \
+                                       -j 1
+
+    log_Msg 3 `date`
+    log_Msg 3 "Converting the transformation to FSL format"
+
+    ${C3DPATH}/c3d_affine_tool -ref ${FSLDIR}/data/standard/MNI152_T1_1mm.nii.gz \
+                               -src ${WD}/T1.nii.gz \
+                               -itk ${WD}/SynOut0GenericAffine.mat \
+                               -ras2fsl \
+                               -o ${WD}/affine_fsl.mat
+
+    /usr/local/c3d/bin/c3d -mcs ${WD}/SynOut1Warp.nii.gz -oo ${WD}/warp_x_tmp.nii.gz ${WD}/warp_y_tmp.nii.gz ${WD}/warp_z_tmp.nii.gz
+    ${FSLDIR}/bin/fslmaths ${WD}/warp_y_tmp -mul -1 ${WD}/warp_y_tmp
+    ${FSLDIR}/bin/fslmerge -t ${WD}/warp_fsl ${WD}/warp_x_tmp ${WD}/warp_y_tmp ${WD}/warp_z_tmp
+
+    ${FSLDIR}/bin/convertwarp --rel -r ${FSLDIR}/data/standard/MNI152_T1_1mm -m ${WD}/affine_fsl.mat -w ${WD}/warp_fsl -o ${WD}/total_warp_fsl
+#    ${FSLDIR}/bin/applywarp -i ${WD}/T1 -r ${FSLDIR}/data/standard/MNI152_T1_1mm -w ${WD}/total_warp_fsl -o ${WD}/T1w_warped
+    ${FSLDIR}/bin/invwarp -r ${WD}/T1 -w ${WD}/total_warp_fsl -o ${WD}/T1_to_MNI_nonlin_coeff_inv
+
+    ${FSLDIR}/bin/immv ${WD}/total_warp_fsl ${WD}/T1_to_MNI_nonlin_field
+    mv ${WD}/affine_fsl.mat ${WD}/T1_to_MNI_linear.mat
+
+#    printf "3" > ${LogFile%/*}/RegType.txt
 fi
 
 log_Msg 3 `date`
 log_Msg 3 "Performing brain extraction"
-if [ $RegType == 2 ]; then
-
-    ${FSLDIR}/bin/invwarp --ref=${WD}/T1 -w ${WD}/T1_to_MNI_nonlin_coeff -o ${WD}/T1_to_MNI_nonlin_coeff_inv
-    ${FSLDIR}/bin/applywarp --rel --interp=trilinear --in=${BRC_GLOBAL_DIR}/templates/MNI152_T1_1mm_brain_mask --ref=${WD}/T1 -w ${WD}/T1_to_MNI_nonlin_coeff_inv -o ${WD}/T1_brain_mask
-    ${FSLDIR}/bin/fslmaths ${WD}/T1 -mul ${WD}/T1_brain_mask ${WD}/T1_brain
-#    ${FSLDIR}/bin/fslmaths ${WD}/T1_brain_to_MNI -mul ${BRC_GLOBAL_DIR}/templates/MNI152_T1_1mm_brain_mask ${WD}/T1_brain_to_MNI
-
-elif [ $RegType == 1 ]; then
+if [ $RegType == 1 ]; then
 
     ${FSLDIR}/bin/imcp ${WD}/T1_tmp_brain_mask ${WD}/T1_brain_mask
     ${FSLDIR}/bin/flirt -interp spline -in ${WD}/T1_tmp -ref ${WD}/T1 -omat ${WD}/T1_tmp_to_T1.mat -out ${WD}/T1_tmp_to_T1
     ${FSLDIR}/bin/applywarp --rel --interp=nn -i ${WD}/T1_brain_mask -r ${WD}/T1 --premat=${WD}/T1_tmp_to_T1.mat -o ${WD}/T1_brain_mask
     ${FSLDIR}/bin/fslmaths ${WD}/T1 -mul ${WD}/T1_brain_mask ${WD}/T1_brain
+
+elif [ $RegType == 2 ] || [ $RegType == 3 ]; then
+
+    if [ $RegType == 2 ]; then
+        ${FSLDIR}/bin/invwarp --ref=${WD}/T1 -w ${WD}/T1_to_MNI_nonlin_coeff -o ${WD}/T1_to_MNI_nonlin_coeff_inv
+    fi
+    ${FSLDIR}/bin/applywarp --rel --interp=trilinear --in=${BRC_GLOBAL_DIR}/templates/MNI152_T1_1mm_brain_mask --ref=${WD}/T1 -w ${WD}/T1_to_MNI_nonlin_coeff_inv -o ${WD}/T1_brain_mask
+    ${FSLDIR}/bin/fslmaths ${WD}/T1 -mul ${WD}/T1_brain_mask ${WD}/T1_brain
+#    ${FSLDIR}/bin/fslmaths ${WD}/T1_brain_to_MNI -mul ${BRC_GLOBAL_DIR}/templates/MNI152_T1_1mm_brain_mask ${WD}/T1_brain_to_MNI
+
+#elif [ $RegType == 3 ]; then
+
+#    ${ANTSPATH}/antsApplyTransforms -d 3 -n NearestNeighbor -i ${BRC_GLOBAL_DIR}/templates/MNI152_T1_1mm_brain_mask.nii.gz -r ${WD}/T1.nii.gz -o ${WD}/T1_brain_mask.nii.gz -t [${WD}/T1_to_MNI_nonlin0GenericAffine.mat,1] -t [${WD}/T1_to_MNI_nonlin_coeff_inv.nii.gz]
+#    ${FSLDIR}/bin/fslmaths ${WD}/T1 -mul ${WD}/T1_brain_mask ${WD}/T1_brain
+
+#    ${FSLDIR}/bin/applywarp --rel --interp=trilinear --in=${BRC_GLOBAL_DIR}/templates/MNI152_T1_1mm_brain_mask --ref=${WD}/T1 -w ${WD}/T1_to_MNI_nonlin_coeff_inv -o ${WD}/T1_brain_mask
+#    ${FSLDIR}/bin/fslmaths ${WD}/T1 -mul ${WD}/T1_brain_mask ${WD}/T1_brain
+
+#    ${ANTSPATH}/antsApplyTransforms -d 3 -n BSpline -i ${WD}/T1_brain.nii.gz -r ${FSLDIR}/data/standard/MNI152_T1_1mm.nii.gz -o ${WD}/T1_brain_2_MNI_linear.nii.gz -t [${WD}/T1_to_MNI_nonlin0GenericAffine.mat,0]
+#    ${ANTSPATH}/antsApplyTransforms -d 3 -n BSpline -i ${WD}/T1.nii.gz -r ${FSLDIR}/data/standard/MNI152_T1_1mm.nii.gz -o ${WD}/T1_2_MNI_linear.nii.gz -t [${WD}/T1_to_MNI_nonlin0GenericAffine.mat,0]
+#
+#    ${ANTSPATH}/antsApplyTransforms -d 3 -n BSpline -i ${WD}/T1_brain.nii.gz -r ${FSLDIR}/data/standard/MNI152_T1_1mm.nii.gz -o ${WD}/T1_brain_2_MNI_nonlin.nii.gz -t [${WD}/T1_to_MNI_nonlin1Warp.nii.gz] -t [${WD}/T1_to_MNI_nonlin0GenericAffine.mat,0]
+#    ${ANTSPATH}/antsApplyTransforms -d 3 -n BSpline -i ${WD}/T1.nii.gz -r ${FSLDIR}/data/standard/MNI152_T1_1mm.nii.gz -o ${WD}/T1_2_MNI_nonlin.nii.gz -t [${WD}/T1_to_MNI_nonlin1Warp.nii.gz] -t [${WD}/T1_to_MNI_nonlin0GenericAffine.mat,0]
 
 fi
 
@@ -157,7 +210,11 @@ if [ $do_defacing = "yes" ] ; then
 fi
 
 #Clean and reorganize
-#rm ${WD}/*tmp*
+if [ $RegType == 3 ]; then
+    rm ${WD}/*_fsl*
+    rm ${WD}/SynOut*
+fi
+rm ${WD}/*tmp*
 if [ -e ${regTempT1Folder} ] ; then rm -r ${regTempT1Folder}; fi; mkdir ${regTempT1Folder}
 mv ${WD}/*MNI* ${regTempT1Folder}
 ##mv ${WD}/*warp*.* ${regTempT1Folder}
@@ -191,19 +248,40 @@ else
 fi
 
 #${FSLDIR}/bin/applywarp --rel -i ${WD}/T1_unbiased -r $FSLDIR/data/standard/MNI152_T1_1mm -o ${regTempT1Folder}/T1_to_MNI_linear --premat=${regTempT1Folder}/T1_to_MNI_linear.mat --interp=spline
-${FSLDIR}/bin/flirt -interp spline -dof 12 -in ${WD}/T1_unbiased -ref $FSLDIR/data/standard/MNI152_T1_1mm -omat ${regTempT1Folder}/T1_to_MNI_linear_temp.mat -out ${regTempT1Folder}/T1_to_MNI_linear
+log_Msg 3  `date`
+log_Msg 3 "Apply linear and non-linear registration"
+if [ $RegType == 1 ] || [ $RegType == 2 ] ; then
+
+    ${FSLDIR}/bin/flirt -interp spline -dof 12 -in ${WD}/T1_unbiased -ref $FSLDIR/data/standard/MNI152_T1_1mm -omat ${regTempT1Folder}/T1_to_MNI_linear.mat -out ${regTempT1Folder}/T1_to_MNI_linear
+    ${FSLDIR}/bin/applywarp --rel --interp=spline -i ${WD}/T1_unbiased_brain -r $FSLDIR/data/standard/MNI152_T1_1mm_brain --premat=${regTempT1Folder}/T1_to_MNI_linear.mat -o ${regTempT1Folder}/T1_brain_to_MNI_linear
+
+elif [ $RegType == 3 ]; then
+
+#    ${ANTSPATH}/antsApplyTransforms -d 3 -n BSpline -i ${WD}/T1_unbiased.nii.gz -r ${FSLDIR}/data/standard/MNI152_T1_1mm.nii.gz -o ${regTempT1Folder}/T1_to_MNI_linear.nii.gz -t [${regTempT1Folder}/T1_to_MNI_nonlin0GenericAffine.mat,0]
+#    ${ANTSPATH}/antsApplyTransforms -d 3 -n BSpline -i ${WD}/T1_unbiased_brain.nii.gz -r ${FSLDIR}/data/standard/MNI152_T1_1mm.nii.gz -o ${regTempT1Folder}/T1_brain_to_MNI_linear.nii.gz -t [${regTempT1Folder}/T1_to_MNI_nonlin0GenericAffine.mat,0]
+
+    ${FSLDIR}/bin/applywarp --rel --interp=spline -i ${WD}/T1_unbiased -r $FSLDIR/data/standard/MNI152_T1_1mm --premat=${regTempT1Folder}/T1_to_MNI_linear.mat -o ${regTempT1Folder}/T1_to_MNI_linear
+    ${FSLDIR}/bin/applywarp --rel --interp=spline -i ${WD}/T1_unbiased_brain -r $FSLDIR/data/standard/MNI152_T1_1mm --premat=${regTempT1Folder}/T1_to_MNI_linear.mat -o ${regTempT1Folder}/T1_brain_to_MNI_linear
+
+fi
 # Remove negative intensity values (from eddy) from final data
 ${FSLDIR}/bin/fslmaths ${regTempT1Folder}/T1_to_MNI_linear -thr 0 ${regTempT1Folder}/T1_to_MNI_linear
+${FSLDIR}/bin/fslmaths ${regTempT1Folder}/T1_brain_to_MNI_linear -thr 0 ${regTempT1Folder}/T1_brain_to_MNI_linear
 
-${FSLDIR}/bin/applywarp --rel --interp=spline -i ${WD}/T1_unbiased_brain -r $FSLDIR/data/standard/MNI152_T1_1mm_brain --premat=${regTempT1Folder}/T1_to_MNI_linear.mat -o ${regTempT1Folder}/T1_to_MNI_brain_linear
+#args=""
+#if [ $RegType == 2 ] ; then
+#    args=" --rel"
+#fi
+#args=" --rel"
 
-if [ $RegType == 2 ]; then
+if [ $RegType == 2 ] || [ $RegType == 3 ] ; then
 
     ${FSLDIR}/bin/applywarp --rel -i ${WD}/T1_unbiased -r $FSLDIR/data/standard/MNI152_T1_1mm -o ${regTempT1Folder}/T1_to_MNI_nonlin -w ${regTempT1Folder}/T1_to_MNI_nonlin_field --interp=spline
     ${FSLDIR}/bin/applywarp --rel -i ${WD}/T1_unbiased_brain -r $FSLDIR/data/standard/MNI152_T1_1mm_brain -o ${regTempT1Folder}/T1_brain_to_MNI_nonlin -w ${regTempT1Folder}/T1_to_MNI_nonlin_field --interp=spline
 
     ${FSLDIR}/bin/fslmaths ${regTempT1Folder}/T1_to_MNI_nonlin -thr 0 ${regTempT1Folder}/T1_to_MNI_nonlin
     ${FSLDIR}/bin/fslmaths ${regTempT1Folder}/T1_brain_to_MNI_nonlin -thr 0 ${regTempT1Folder}/T1_brain_to_MNI_nonlin
+
 fi
 
 if [ $dosubseg = "yes" ] ; then
@@ -218,7 +296,7 @@ if [ $dosubseg = "yes" ] ; then
     ${FSLDIR}/bin/run_first_all -i ${FirstT1Folder}/T1_unbiased_brain -b -o ${FirstT1Folder}/T1_first
 fi
 
-if [ $RegType == 2 ]; then
+if [ $RegType == 2 ] || [ $RegType == 3 ]; then
 
     log_Msg 3  `date`
     log_Msg 3 "Run Sienax"
@@ -239,4 +317,10 @@ log_Msg 3 "                             ===============                         
 ################################################################################################
 ## Cleanup
 ################################################################################################
+#if [ $RegType == 3 ]; then
+#    rm ${regTempT1Folder}/T1_to_MNI_nonlin0GenericAffine.mat
+#    $FSLDIR/bin/imrm ${regTempT1Folder}/T1_to_MNI_nonlinWarped.nii.gz
+#    $FSLDIR/bin/imrm ${regTempT1Folder}/T1_to_MNI_nonlin1Warp.nii.gz
+#    $FSLDIR/bin/imrm ${regTempT1Folder}/T1_to_MNI_nonlin1InverseWarp.nii.gz
+#fi
 #: <<'COMMENT'
